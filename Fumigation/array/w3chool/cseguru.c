0fcae43d74c146815bb2201b37be63b1b932063b
Akhil Anwer <40333606+akhil1234A@users.noreply.github.com>
2024-05-15 14:50:57 +0530

    Update cseguru.c

--- Fumigation/array/w3chool/cseguru.c
+++ Fumigation/array/w3chool/cseguru.c
@@ -61,3 +61,42 @@ for(int i=0; i<size; i++){
 
 return 0; 
 }
+
+//print in reverse order
+#include <stdio.h>
+
+int main() 
+{
+
+int arr[] = {1,2,3,4,5};
+int size = sizeof(arr)/sizeof(arr[0]);
+
+for(int i=size-1; i>=0; i--){
+    printf("%d ",arr[i]);
+}
+
+return 0; 
+}
+//reverse an array
+#include <stdio.h>
+
+int main() 
+{
+
+int arr[] = {1,2,3,4,5,6};
+int size = sizeof(arr)/sizeof(arr[0]);
+
+int temp;
+
+for(int i=0; i<(size/2); i++){
+    temp = arr[i];
+    arr[i] = arr[size-1-i];
+    arr[size-1-i]=temp;
+}
+
+for(int i=0; i<size; i++){
+    printf("%d ",arr[i]);
+}
+
+return 0; 
+}
