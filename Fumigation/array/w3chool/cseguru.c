b7460c18c48b2c741d068da3b9552b17c59f201f
Akhil Anwer <40333606+akhil1234A@users.noreply.github.com>
2024-05-15 14:07:35 +0530

    Create cseguru.c

---
+++ Fumigation/array/w3chool/cseguru.c
@@ -0,0 +1,29 @@
+//insertion at given position
+#include <stdio.h>
+
+int main() 
+{
+
+int arr[] = {1,2,3,4,5};
+int size = sizeof(arr)/sizeof(arr[0]);
+
+int pos,num; 
+//taking values from user
+printf("enter the position: ");
+scanf("%d",&pos);
+printf("enter the number to insert: ");
+scanf("%d",&num); 
+
+
+for(int i=pos-1; i>=pos; i--)
+    arr[i+1]=arr[i];
+arr[pos]=num;
+size++;
+
+for(int i=0; i<size; i++){
+	printf("%d ",arr[i]);
+}
+
+
+return 0; 
+}
