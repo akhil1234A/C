c8d47cbe1feccefc237d18d4dbf4f28f80335c66
Akhil Anwer <40333606+akhil1234A@users.noreply.github.com>
2024-05-15 14:29:45 +0530

    Update cseguru.c

--- Fumigation/array/w3chool/cseguru.c
+++ Fumigation/array/w3chool/cseguru.c
@@ -1,4 +1,4 @@
-//insertion at given position
+//insert an element at given position 
 #include <stdio.h>
 
 int main() 
@@ -14,11 +14,45 @@ scanf("%d",&pos);
 printf("enter the number to insert: ");
 scanf("%d",&num); 
 
+// given index
+// for(int i=size-1; i>=pos; i--)
+//     arr[i+1]=arr[i];
+// arr[pos]=num;
+// size++;
 
-for(int i=size-1; i>=pos; i--)
+//given position not index
+for(int i=size-1; i>=pos-1; i--)
     arr[i+1]=arr[i];
-arr[pos]=num;
+arr[pos-1]=num;
 size++;
+
+	
+for(int i=0; i<size; i++){
+	printf("%d ",arr[i]);
+}
+
+
+return 0; 
+}
+//delete an element from given position 
+#include <stdio.h>
+
+int main() 
+{
+
+int arr[] = {1,2,3,4,5};
+int size = sizeof(arr)/sizeof(arr[0]);
+
+int pos,num; 
+//taking values from user
+printf("enter the position to delete: ");
+scanf("%d",&pos);
+
+
+
+for(int i=pos-1; i<size; i++)
+    arr[i]=arr[i+1];
+size--;
 
 for(int i=0; i<size; i++){
 	printf("%d ",arr[i]);
